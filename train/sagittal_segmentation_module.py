5b9f61f64b35af7bd8ad96cbbe73a91febb10dca
Nadja1611 <61620034+Nadja1611@users.noreply.github.com>
2022-05-24 10:41:44 +0200

    Update sagittal_segmentation_module.py

--- train/sagittal_segmentation_module.py
+++ train/sagittal_segmentation_module.py
@@ -6,8 +6,8 @@ from functions import *
 
 os.chdir(".\\Data_npz")
 data = np.load("Training_Babies.npz")
-X = data["T1"]
-Y = data["Labels"]
+X_train = data["T1"]
+Y_train = data["Labels"]
 Labels=np.copy(Y)
 indices = np.load("indices.npz")
 indices = indices["indices"]
@@ -18,8 +18,8 @@ thalamus = Thalamus["thalamus"]
 
 
 os.chdir('.\\')
-X = preprocessing_sagittal_crossvalidation(X,Y,indices,thalamus)[0]
-Y=preprocessing_sagittal_crossvalidation(X,Y,indices,thalamus)[1]
+X = preprocessing_sagittal_crossvalidation(X_train,Y_train,indices,thalamus)[0]
+Y=preprocessing_sagittal_crossvalidation(X_train, Y_train,indices,thalamus)[1]
 
 
 
